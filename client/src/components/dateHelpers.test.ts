4b4296cfec05a383227520d4c9fd94c219ea21d7
Roman Morozov <roman.morozov@bsci.com>
2019-07-16 14:08:59 -0700

    test(config) adjust tsconfig to stop receiving ts error for describe

---
+++ client/src/components/dateHelpers.test.ts
@@ -0,0 +1,8 @@
+
+describe('date helpers', () => {
+  describe('generateCalendarLayout', () => {
+    it('should generate calendar layout for valid year, month', () => {
+      expect(1).toBe(2)
+    })
+  })
+})
